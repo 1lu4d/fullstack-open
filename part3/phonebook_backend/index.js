854e207fcc733419e14e7aba36e3e5f15edd2c6e
1lu4d <135172958+1lu4d@users.noreply.github.com>
2026-07-27 16:10:48 +0300

    PART3: 3.7 - Morgan Freeman

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const http = require("http");
 const app = express();
+var morgan = require("morgan");
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
-let notes = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -27,9 +29,15 @@ let notes = [
   },
 ];
 
-app.get("/api/notes/:id", (request, response) => {
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
+  const note = persons.find((note) => note.id === id);
 
   if (note) {
     response.json(note);
@@ -38,35 +46,38 @@ app.get("/api/notes/:id", (request, response) => {
   }
 });
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
-
-app.post("/api/notes", (request, response) => {
+app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
+  console.log(body);
+
+  if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "content missing",
+      error: "The name or number is missing",
+    });
+  }
+
+  if (persons.some((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "The name already exists in phonebook",
     });
   }
 
   const note = {
-    content: body.content,
-    important: body.important || false,
     id: generateId(),
+    name: body.name,
+    number: body.number,
   };
 
-  notes = notes.concat(note);
+  persons = persons.concat(note);
 
   response.json(note);
 });
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
+  persons = persons.filter((note) => note.id !== id);
+  console.log(persons);
 
   response.status(204).end();
 });
@@ -75,11 +86,25 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date();
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
+    `);
 });
 
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
