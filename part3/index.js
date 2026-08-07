0176ecbab899f58a3a35973d45338b38be26e244
1lu4d <135172958+1lu4d@users.noreply.github.com>
2026-08-07 18:15:59 +0300

    PART3: 3.15-3.18 + added favicon.ico

--- part3/index.js
+++ part3/index.js
@@ -4,6 +4,7 @@ const http = require("http");
 const cors = require("cors");
 const Person = require("./models/person");
 var morgan = require("morgan");
+const path = require("path");
 const app = express();
 
 morgan.token("body", (req) => {
@@ -14,49 +15,31 @@ morgan.token("body", (req) => {
 });
 
 app.use(cors());
-app.use(express.json());
 app.use(express.static("dist"));
+app.use(express.json());
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms Data: :body",
   ),
 );
 
+// Pour le hampteur
+app.get("/favicon.svg", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "favicon.ico"));
+});
+
 const errorHandler = (error, request, response, next) => {
   console.error("error.name:", error.name);
   console.error("error.message:", error.message);
 
   if (error.name === "CastError") {
-    return response.status(400).json({ error: "Wrong id format" });
+    return response.status(400).send({ error: "Wrong id format" });
   } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).send({ error: error.message });
   }
 
   return response.status(500).json({ error: "Internal server error" });
 };
-
-// let persons = [
-//   {
-//     id: "1",
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: "2",
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: "3",
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: "4",
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-// ];
 
 app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
@@ -79,51 +62,41 @@ app.post("/api/persons", (request, response, next) => {
     });
   }
 
-  Person.findOne({ name: body.name })
-    .then((existingPerson) => {
-      if (existingPerson) {
-        return response.status(400).json({
-          error: "The name already exists in phonebook",
-        });
-      }
-      const person = new Person({
-        name: body.name,
-        number: body.number,
-      });
-      return person.save();
-    })
-    .then((savedPerson) => {
-      if (savedPerson) {
-        response.json(savedPerson);
-      }
+  Person.findOneAndUpdate(
+    { name: body.name },
+    { name: body.name, number: body.number },
+    {
+      new: true,
+      upsert: true,
+      runValidators: true,
+      context: "query",
+    },
+  )
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
     })
     .catch((error) => next(error));
 });
 
-// Definitelly nothing to see here (It will be back)
+// Its back from the dead
 
-// app.put("/api/persons/:id", (request, response) => {
-//   const id = request.params.id;
-//   const body = request.body;
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
 
-//   const person = persons.find((person) => person.id === id);
-
-//   console.log(person);
-
-//   if (person) {
-//     const updatedPerson = {
-//       ...person,
-//       number: body.number,
-//     };
-
-//     persons = persons.map((person) =>
-//       person.id !== id ? person : updatedPerson,
-//     );
-//     response.json(updatedPerson);
-//   } else {
-//     response.status(404).end();
-//   }
-// });
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" },
+  )
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        response.json(updatedPerson);
+      } else {
+        response.status(404).json({ error: "person not found" });
+      }
+    })
+    .catch((error) => next(error));
+});
 
 app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
@@ -148,7 +121,7 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/info", (request, response, next) => {
-  Person.countDocuments({})
+  Person.estimatedDocumentCount()
     .then((count) => {
       const date = new Date();
       response.send(`
